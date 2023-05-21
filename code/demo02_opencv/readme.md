88d5a657a9f660b33e45df2604424f35f97d90c9
panda <93306769+xiongzuming@users.noreply.github.com>
2023-05-21 21:16:38 +0800

    2
    2

---
+++ code/demo02_opencv/readme.md
@@ -0,0 +1,19 @@
+### 彩图转灰度图
+
+```
+roslaunch usb_cam usb_cam-test.launch
+rosrun demo02_opencv image_to_grey
+```
+### 彩图转灰二值图
+
+```
+roslaunch usb_cam usb_cam-test.launch
+rosrun demo02_opencv image_to_binary
+```
+### 检测人脸位置
+
+```
+roslaunch usb_cam usb_cam-test.launch
+rosrun demo02_opencv image_face_detect
+```
+
---
+++ code/demo02_opencv/src/image_to_binary.cpp
@@ -0,0 +1,74 @@
+#include <ros/ros.h>
+#include <opencv2/core.hpp>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace cv;
+static const std::string OPENCV_WINDOW = "Image window";
+
+class ImageConverter
+{
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber image_sub_;
+    image_transport::Publisher image_pub_;
+
+public:
+    ImageConverter()
+        : it_(nh_)
+    {
+        // Subscrive to input video feed and publish output video feed
+        image_sub_ = it_.subscribe("/usb_cam/image_raw", 1,
+                                   &ImageConverter::imageCb, this);
+        image_pub_ = it_.advertise("/image_converter/output_video", 1);
+
+        cv::namedWindow(OPENCV_WINDOW,WINDOW_AUTOSIZE);
+    }
+
+    ~ImageConverter()
+    {
+        cv::destroyWindow(OPENCV_WINDOW);
+    }
+
+    void imageCb(const sensor_msgs::ImageConstPtr &msg)
+    {
+        cv_bridge::CvImagePtr cv_ptr;
+        try
+        {
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        }
+        catch (cv_bridge::Exception &e)
+        {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+            return;
+        }
+
+        // write what you want to do!
+        Mat grey;
+        Mat bina;
+        if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
+            // cv::circle(cv_ptr->image, cv::Point(240, 240), 100, CV_RGB(255, 0, 0),5);
+        {
+            cv::cvtColor(cv_ptr->image,grey,COLOR_BGRA2GRAY);
+            cv::threshold(grey,bina,122,255,cv::THRESH_BINARY);
+        }
+
+        // Update GUI Window
+        cv::imshow(OPENCV_WINDOW, bina);
+        cv::waitKey(3);
+
+        // Output modified video stream
+        image_pub_.publish(cv_ptr->toImageMsg());
+    }
+};
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "image_converter");
+    ImageConverter ic;
+    ros::spin();
+    return 0;
+}
