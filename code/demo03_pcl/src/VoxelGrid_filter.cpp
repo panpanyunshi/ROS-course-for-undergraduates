f8bc3e971422b6b5e9e9e82ac3ee451919054c9f
Bonen <97874549+Bowen86086@users.noreply.github.com>
2023-05-25 19:13:57 +0800

    Update VoxelGrid_filter.cpp

--- code/demo03_pcl/src/VoxelGrid_filter.cpp
+++ code/demo03_pcl/src/VoxelGrid_filter.cpp
@@ -17,34 +17,34 @@ cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
   pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
   pcl::PCLPointCloud2 cloud_filtered;
 
-  // to PCL data
+  // To PCL data
   pcl_conversions::toPCL(*cloud_msg, *cloud);
 
-  // creat a VoxelGrid filter, size = 0.1m
+  // Creat a VoxelGrid filter, size = 0.1m
   pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
   sor.setInputCloud (cloudPtr);
   sor.setLeafSize (0.1, 0.1, 0.1);
   sor.filter (cloud_filtered);
 
-  // to ROS data
+  // To ROS data
   sensor_msgs::PointCloud2 output;
   pcl_conversions::moveFromPCL(cloud_filtered, output);
 
-  // publish filtered point cloud data
+  // Publish filtered point cloud data
   pub.publish (output);
 }
 
 int
 main (int argc, char** argv)
 {
-  // initialize ROS node
+  // Initialize ROS node
   ros::init (argc, argv, "my_pcl_tutorial");
   ros::NodeHandle nh;
 
-  // creat a subscriber
+  // Creat a subscriber
   ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2> ("velodyne_points", 1, cloud_cb);
 
-  // creat a publisher 
+  // Creat a publisher 
   pub = nh.advertise<sensor_msgs::PointCloud2> ("filtered_points", 1);
 
   // Spin
